3d6d9720420aba7568e308f26f0a08fc8dbb89f5
Kiran Koduru <crazycreature11@gmail.com>
2018-10-01 00:54:33 -0400

    Sample spider code

--- sample_project/sample_project/spiders/test_spider.py
+++ sample_project/sample_project/spiders/test_spider.py
@@ -4,8 +4,14 @@ import scrapy
 
 class TestSpiderSpider(scrapy.Spider):
     name = 'test_spider'
-    allowed_domains = ['http://books.toscrape.com/']
-    start_urls = ['http://http://books.toscrape.com//']
+    allowed_domains = ['books.toscrape.com/']
+    start_urls = ['http://books.toscrape.com//']
 
     def parse(self, response):
-        pass
+        items = response.css('ol.row li.col-xs-6')
+        for item in items:
+            yield {
+                'title': item.css('.product_pod h3 a::text').extract_first(),
+                'price': item.css('.product_pod .product_price .price_color::text').extract_first(),
+                'availability': item.css('.product_pod .product_price .availability::text').extract()[1].strip(),
+            }
